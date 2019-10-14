e53617259f9bed2438e0e3997573f1c06b3e4e8f
Rogue Bell <mattbellantoni3@gmail.com>
2019-10-14 12:40:01 +0200

    creating todos component

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos"
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.5;
+  background: #585252;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <Todos />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Todos from "@/components/Todos.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Todos
   }
 };
 </script>
